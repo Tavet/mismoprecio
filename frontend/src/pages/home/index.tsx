c5ea9169c1d897c98ee86a741bbc4c1c50b0e65d
Breyner <mon2bdr@gmail.com>
2021-01-11 21:14:44 -0500

    [Test] Añadir título diferente para probar ambiente de desarrollo

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -8,7 +8,7 @@ const WithStaticProps = () => (
             <Container>
                 <Row>
                     <Col xs="6" md="7">
-                        <h1>No es una tienda, <br />Es tu sitio de ropa preferido</h1>
+                        <h1>RAMA FEATURE-FRONTEND PAPA JIJI</h1>
                         <p>Podrás encontrar y comparar prendas de distintas tiendas en un mismo lugar y al mismo precio de tu bolsillo</p>
                         <Button color="primary">Ver catálogo</Button>
                     </Col>
